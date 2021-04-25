1a8820eb608f67bba0067a94ff24554b136921f4
HongKeuni <hongkeun@sixshop.com>
2021-04-25 21:53:07 +0900

    Todo list 수정하기

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -1,7 +1,41 @@
 <script>
     export let todo;
+
+    let isEdit = false;
+    let title = '';
+
+    function onEdit() {
+        isEdit = true;
+        title = todo.title;
+    }
+
+    function offEdit() {
+        isEdit = false;
+    }
+
+    function updateTodo() {
+        todo.title = title;
+        offEdit();
+    }
+
+    function onDelete() {
+
+    }
 </script>
 
-<div>
-    {todo.title}
-</div>
+
+{#if isEdit}
+    <div>
+        <input type="text" 
+            bind:value={title}
+            on:keydown={(e) => {e.key === 'Enter' && updateTodo}} />
+        <button on:click={updateTodo}>Ok</button>
+        <button on:click={offEdit}>Cancel</button>
+    </div>
+{:else}
+    <div>
+        {todo.title}
+        <button on:click={onEdit}>Edit</button>
+        <button on:click={onDelete}>Delete</button>
+    </div>
+{/if}
