6c1a0259b26ab0701f61f7daf6b11e97bf240a9a
HongKeuni <hongkeun@sixshop.com>
2021-04-25 16:52:37 +0900

    선언적 렌더링

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,21 @@
 <script>
-	export let name;
+	let name = 'world';
+	let age = 85;
+
+	function assign() {
+		name = 'Keu.ni',
+		age = 30
+	}
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h2 class={age < 85 ? 'active' : null}>
+		{age}
+	</h2>
+	<img src="" alt={name}>
+	<input type="text" bind:value={name}>
+	<button on:click={assign}>Assign</button>
 </main>
 
 <style>
@@ -20,6 +31,10 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+	
+	.active {
+		color: #41E382;
 	}
 
 	@media (min-width: 640px) {
