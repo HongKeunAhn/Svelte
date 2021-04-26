dac61d9d083ae7ae1d100dd3c753264817b581d7
Keu-ni <skcor8909@gmail.com>
2021-04-26 20:57:40 +0900

    onMount, onDestroy

---
+++ src/Something.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { onDestroy, onMount } from 'svelte';
+  
+  onMount(() => {
+      console.log('onMounted!');
+    //   const h1 = document.querySelector('h1');
+    //   console.log(h1.innerText); 
+
+      return () => {
+          console.log('Destory in mount');
+      }
+  });
+
+  onDestroy(() => {
+    console.log('Before destroy~');
+    const h1 = document.querySelector('h1');
+      console.log(h1.innerText); 
+  });
+
+</script>
+
+<h1>Something...</h1> 
+ 
