45f99991255d4e2b0e61a753877c3c4defd1afdb
Keu-ni <skcor8909@gmail.com>
2021-04-27 08:28:24 +0900

    beforeUpdate, afterUpdate

--- src/Something.svelte
+++ src/Something.svelte
@@ -1,23 +1,33 @@
 <script>
-  import { onDestroy, onMount } from 'svelte';
-  
-  onMount(() => {
-      console.log('onMounted!');
-    //   const h1 = document.querySelector('h1');
-    //   console.log(h1.innerText); 
+  import { afterUpdate, beforeUpdate, onDestroy, onMount } from 'svelte';
 
-      return () => {
-          console.log('Destory in mount');
-      }
+  let name = 'Something..';
+  let h1;
+
+  function moreDot() {
+    name += '.';
+  }
+
+  beforeUpdate(() => {
+    // console.log('Before update!');
+    console.log('beforeUpdate', h1 && h1.innerText); // undefind
+  });
+
+  afterUpdate(() => {
+    // console.log('After Update!');
+    console.log('afterUpdate', h1.innerText);
+  });
+
+  onMount(() => {
+    console.log('onMounted!');
+    h1 = document.querySelector('h1');
   });
 
   onDestroy(() => {
-    console.log('Before destroy~');
+    // console.log('Before destroy~');
     const h1 = document.querySelector('h1');
-      console.log(h1.innerText); 
+    console.log('onDestroy', h1.innerText);
   });
-
 </script>
 
-<h1>Something...</h1> 
- 
+<h1 on:click={moreDot}>{name}</h1>
