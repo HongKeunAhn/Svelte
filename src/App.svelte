3d54f4c2cc80411dda2bd2ad418dd5bcc3bfc587
Keu-ni <skcor8909@gmail.com>
2021-05-16 21:09:51 +0900

    일반 요소 바인딩 (this)

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,36 @@
 <script>
-	export let name;
+  import { tick } from 'svelte';
+
+  let isShow = false;
+  let inputEl;
+
+  async function toggle() {
+    isShow = !isShow;
+    await tick();
+    // const inputEl = document.querySelector('input');
+    inputEl && inputEl.focus();
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <button on:click={toggle}>Edit!</button>
+
+  {#if isShow}
+    <input type="text" bind:this={inputEl} />
+  {/if}
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
