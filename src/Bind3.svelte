4ed8eca5033840cff230b5d8f1e0488fd057a755
Keu-ni <skcor8909@gmail.com>
2021-05-16 22:09:31 +0900

    요소 바인딩 파일 분리 정리2

---
+++ src/Bind3.svelte
@@ -0,0 +1,32 @@
+<script>
+  let innerHTML = '';
+  let textContent = 'Hello world!';
+</script>
+
+<main>
+  <div contenteditable bind:innerHTML bind:textContent />
+
+  <div>{innerHTML}</div>
+  <div>{textContent}</div>
+  <div>{@html innerHTML}</div>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  div {
+    border: 1px solid royalblue;
+    margin-bottom: 10px;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
