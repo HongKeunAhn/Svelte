77fbabf245c2daef1088b018e8a1807c92e9b8d6
Keu-ni <skcor8909@gmail.com>
2021-05-16 20:30:28 +0900

    스타일 유효범위 & 전역화

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,16 @@
 <script>
-  let active = true;
-  let valid = false;
-  let camelCase = true;
-
-  function multi() {
-    return 'active multiple-class';
-  }
+  import Fruits from './Fruits.svelte';
 </script>
 
 <main>
-  <div class={active ? 'active' : ''}>3항 연산자 보간</div>
+  <h2>App.svelte</h2>
+  <ul class="fruits">
+    <li>Apple</li>
+    <li>Banana</li>
+    <li>Cherry</li>
+  </ul>
 
-  <!-- active는 클래스 이름, {active} 데이터 -->
-  <div class:active={active}>Class 지시어(Directive) 바인딩</div>
-
-  <!-- 단축어 사용 조건: 클래스 이름과 데이터 이름이 같을 때만 생략 가능하다! -->
-  <div class:active>Class 지시어 바인딩 단축 형태</div>
-
-  <div class:active class:valid class:camelCase class:camel-case={camelCase}>
-    다중(Mutiple) Class 지시어 바인딩
-  </div>
-
-  <!-- 함수는 실행되어야 한다. -->
-  <div class={multi()}>함수 실행</div>
+  <Fruits />
 </main>
 
 <style>
@@ -31,6 +19,14 @@
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
+  }
+
+  :global(.fruits) {
+    color: red;
+  }
+
+  .heropy {
+    color: orange;
   }
   @media (min-width: 640px) {
     main {
