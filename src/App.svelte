21801b0a00d7df875ddcc39d90ba1d88efcedb23
Keu-ni <skcor8909@gmail.com>
2021-05-16 19:55:34 +0900

    클래스와 스타일 속성 바인딩

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,55 @@
 <script>
-	export let name;
+  let active = false;
+  let color = {
+    t: 'tomato',
+    w: '#FFF',
+  };
+  let letterSpacing = 'letter-spacing: 5px;';
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <button
+    on:click={() => {
+      active = !active;
+    }}>Toggle!</button
+  >
+
+  <!-- <div class={active ? 'hello' : ''}>Hello</div> -->
+  <!-- class - 스벨트에서 제공하는 스벨트 지시어 -->
+  <div class:hello={active}>Hellow</div>
+
+  <h2 style="background-color: {color.t}; color: {color.w}; {letterSpacing}">Keuni</h2>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  div {
+    width: 120px;
+    height: 200px;
+    background-color: royalblue;
+    border-radius: 10px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    color: white;
+    transition: 0.4s;
+  }
+  .hello {
+    width: 250px;
+    background-color: tomato;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
