fb844ca0de5c2385798afcd66afcd646a6418b1e
Keu-ni <skcor8909@gmail.com>
2021-05-16 20:10:47 +0900

    스타일 클래스 패턴 정리

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,28 @@
 <script>
-  let active = false;
-  let color = {
-    t: 'tomato',
-    w: '#FFF',
-  };
-  let letterSpacing = 'letter-spacing: 5px;';
+  let active = true;
+  let valid = false;
+  let camelCase = true;
+
+  function multi() {
+    return 'active multiple-class';
+  }
 </script>
 
 <main>
-  <button
-    on:click={() => {
-      active = !active;
-    }}>Toggle!</button
-  >
+  <div class={active ? 'active' : ''}>3항 연산자 보간</div>
 
-  <!-- <div class={active ? 'hello' : ''}>Hello</div> -->
-  <!-- class - 스벨트에서 제공하는 스벨트 지시어 -->
-  <div class:hello={active}>Hellow</div>
+  <!-- active는 클래스 이름, {active} 데이터 -->
+  <div class:active={active}>Class 지시어(Directive) 바인딩</div>
 
-  <h2 style="background-color: {color.t}; color: {color.w}; {letterSpacing}">Keuni</h2>
+  <!-- 단축어 사용 조건: 클래스 이름과 데이터 이름이 같을 때만 생략 가능하다! -->
+  <div class:active>Class 지시어 바인딩 단축 형태</div>
+
+  <div class:active class:valid class:camelCase class:camel-case={camelCase}>
+    다중(Mutiple) Class 지시어 바인딩
+  </div>
+
+  <!-- 함수는 실행되어야 한다. -->
+  <div class={multi()}>함수 실행</div>
 </main>
 
 <style>
@@ -28,25 +32,6 @@
     max-width: 240px;
     margin: 0 auto;
   }
-
-  div {
-    width: 120px;
-    height: 200px;
-    background-color: royalblue;
-    border-radius: 10px;
-    border-radius: 10px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 20px;
-    color: white;
-    transition: 0.4s;
-  }
-  .hello {
-    width: 250px;
-    background-color: tomato;
-  }
-
   @media (min-width: 640px) {
     main {
       max-width: none;
