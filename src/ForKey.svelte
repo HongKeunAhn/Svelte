4ba3727572f2587827258d907cf73aa9ee04361b
Keu-ni <skcor8909@gmail.com>
2021-05-16 22:40:48 +0900

    반복 블록의 key 사용

---
+++ src/ForKey.svelte
@@ -0,0 +1,45 @@
+<script>
+  // let fruits = ['Apple', 'Banana', 'Cherry', 'Orange'];
+  let fruits = [
+    { id: '1', name: 'Apple' },
+    { id: '2', name: 'Banana' },
+    { id: '3', name: 'Cherry' },
+    { id: '4', name: 'Apple' },
+  ];
+
+  function deleteFirst() {
+    fruits = fruits.slice(1); // ['Banana', 'Cherry', 'Orange']
+  }
+</script>
+
+<main>
+  <button on:click={deleteFirst}>Delete first fruit!</button>
+
+  <ul>
+    {#each fruits as fruit (fruit.id)}
+      <li>{fruit.name}</li>
+    {/each}
+  </ul>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
