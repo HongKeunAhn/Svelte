c95f7cc7061b6c0f058b0d2385c1b90727105991
HongKeuni <hongkeun@sixshop.com>
2021-05-19 18:52:00 +0900

    인라인 이벤트 핸들러 별도 파일로 분리

--- src/App.svelte
+++ src/App.svelte
@@ -1,67 +1,98 @@
 <script>
-import { each } from "svelte/internal";
-
-
-	let fruits = [
-		{ id: '1', name: 'Apple' },
-		{ id: '2', name: 'Banana' },
-		{ id: '3', name: 'Cherry' },
-	];
-
-	function assign(fruit) {
-
-	fruit.name += '!';
-	fruits = fruits;
-	// $$invalidate(0. fruits);
+	function clickHandler(event) {
+		// console.log(event.target);
+		console.log(event.currentTarget);
 	}
+
+	function wheelHandler(event) {
+		console.log(event);
+	}
+
 </script>
 
-<main>
-	<section>
-		{#each fruits as fruit }
-			<div on:click={() => assign(fruit)}>
-				{fruit.name}
-			</div>
-		{/each}		
-	</section>
+<section>
+	<!-- 기본 동작 방지 -->
+	<!-- el.addEventListener('click', e => e.preventDefault()) -->
+	<h2>preventDefault</h2>
+	<a href="https://naver.com" 
+		target="_blank" 
+		on:click|preventDefault={clickHandler}>
+		Naver
+	</a>
+</section>
 
-	<section>
-		{#each fruits as fruit (fruit.id) }
-			<div on:click={() => fruit.name += '!'}>
-				{fruit.name}
-			</div>
-		{/each}
-		<!-- $$invalidate(0, each_value_1[fruit_index].name += "!")  -->
-	</section>
+<section>
+	<!-- 최초 실행 후 핸들러 삭제 -->
+	<h2>Once</h2>
+	<a href="https://naver.com" 
+		target="_blank" 
+		on:click|preventDefault|once={clickHandler}>
+		Naver
+	</a>
+</section>
 
-	<section>
-		{#each fruits as {id, name} (id) }
-			<div on:click={() => name += '!'}>
-				{name}
-			</div>
-		{/each}
-		<!-- $$invalidate(0, each_value_1[fruit_index].name += "!")  -->	
-	</section>
-</main>
+<section>
+	<!-- 이벤트 버블링 방지 -->
+	<!-- el.addEventListener('click', e => e.stopPropagation()) -->
+	<h2>stopPropagation</h2>
+	<div class="parent" on:click={clickHandler}>
+		 <div class="child" on:click|stopPropagation={clickHandler}></div>
+	</div>
+</section>
+
+<section>
+	<!-- 이벤트 캡처링에서 핸들러 실행 -->
+	<!-- el.addEventListener('click', e => {}, true) -->
+	<!-- el.addEventListener('click', e => {}, {capture: true}) -->
+	<h2>capture</h2>
+	<!-- child를 클릭하면 parent가 먼저 console에 출력 -->
+	<div class="parent" on:click|capture={clickHandler}>
+		 <div class="child" on:click={clickHandler}></div>
+	</div>
+</section>
+
+<section>
+	<!-- event의 target과 currentTarget이 일치하는 경우 핸들러 실행 -->
+	<h2>self</h2>
+	<!-- self: 자기 자신을 명확하게 클릭했을 때만 실행 event.target = event.currentTarget -->
+	<div class="parent" on:click|self={clickHandler}>
+		 <div class="child"></div>
+	</div>
+</section>
+
+<section>
+	<!-- 이벤트 처리를 완료하지 않고도 기본 속도로 화면을 스크롤 -->
+	<!-- el.addEventListener('wheel', e => {}, {passive: true}) -->
+	<h2>passive</h2>
+	<div class="parent wheel" on:wheel|passive={wheelHandler}>
+		 <div class="child"></div>
+	</div>
+</section>
+
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
 	section {
-		border: 1px solid #ff3e00;
-		text-transform: uppercase;
-		font-size: 2rem;
-		font-weight: 100;
+		border: 1px solid tomato;
+		margin-bottom: 20px;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.parent {
+		width: 100%;
+		height: 300px;
+		background-color: salmon;
+	}
+
+	.child {
+		width: 300px;
+		height: 150px;
+		background-color: yellowgreen;
+	}
+
+	.wheel.parent {
+		overflow: auto;
+	}
+
+	.wheel .child {
+	height: 1000px;
 	}
 </style>
---
+++ src/InlineEventHandler.svelte
@@ -0,0 +1,67 @@
+<script>
+    import { each } from "svelte/internal";
+    
+    
+        let fruits = [
+            { id: '1', name: 'Apple' },
+            { id: '2', name: 'Banana' },
+            { id: '3', name: 'Cherry' },
+        ];
+    
+        function assign(fruit) {
+    
+        fruit.name += '!';
+        fruits = fruits;
+        // $$invalidate(0. fruits);
+        }
+    </script>
+    
+    <main>
+        <section>
+            {#each fruits as fruit }
+                <div on:click={() => assign(fruit)}>
+                    {fruit.name}
+                </div>
+            {/each}		
+        </section>
+    
+        <section>
+            {#each fruits as fruit (fruit.id) }
+                <div on:click={() => fruit.name += '!'}>
+                    {fruit.name}
+                </div>
+            {/each}
+            <!-- $$invalidate(0, each_value_1[fruit_index].name += "!")  -->
+        </section>
+    
+        <section>
+            {#each fruits as {id, name} (id) }
+                <div on:click={() => name += '!'}>
+                    {name}
+                </div>
+            {/each}
+            <!-- $$invalidate(0, each_value_1[fruit_index].name += "!")  -->	
+        </section>
+    </main>
+    
+    <style>
+        main {
+            text-align: center;
+            padding: 1em;
+            max-width: 240px;
+            margin: 0 auto;
+        }
+    
+        section {
+            border: 1px solid #ff3e00;
+            text-transform: uppercase;
+            font-size: 2rem;
+            font-weight: 100;
+        }
+    
+        @media (min-width: 640px) {
+            main {
+                max-width: none;
+            }
+        }
+    </style>
