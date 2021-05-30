5f11b2689809f677170a249757ead396c45c0764
HongKeuni <hongkeun@sixshop.com>
2021-05-30 16:00:08 +0900

    Props 부모에서 자식으로

---
+++ src/Class-2.svelte
@@ -0,0 +1,51 @@
+<script>
+	import User from './User.svelte';
+
+	let users = [
+		{
+			name: 'Neo',
+			age: 85,
+			email: 'neo@abc.com'
+		},
+		{
+			name: 'Lewis',
+			age: 30,
+			email: 'lewis@abc.com'
+		},
+		{
+			name: 'Evan',
+			age: 52,
+		},
+	];
+</script>
+
+<section>
+	{#each users as user}
+		<User
+		name={user.name}
+		age={user.age}
+		email={user.email}
+		/>
+	{/each}
+</section>
+
+<section>
+	{#each users as {name, age, email}}
+		<User {name} {age} {email}/>
+	{/each}
+</section>
+
+<section>
+	{#each users as user}
+		<User {...user}/>
+	{/each}
+</section>
+
+
+
+<style>
+	section {
+		border: 1px solid tomato;
+		margin-bottom: 20px;
+	}
+</style>
