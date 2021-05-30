3ee6b95608f8719ad4ed5d6c2cee6715b800b7da
HongKeuni <hongkeun@sixshop.com>
2021-05-30 15:45:29 +0900

    컴포넌트 기본 개념

---
+++ src/Keuni.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let title = 'Hello';
+
+    let name = 'Keuni';
+    let age = 30;
+    let email = 'skcor8909@gmail.com';
+</script>
+
+<h2>{title}</h2>
+<div>{name}</div>
+<div>{age}</div>
+<div>{email}</div>
